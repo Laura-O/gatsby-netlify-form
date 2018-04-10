4ad4eb4a9c6c45443e58afd01c01384f78fe432b
Laura-O <laura.ohrndorf@gmail.com>
2018-04-10 21:39:07 +0200

    Add honeypot input field

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,13 @@ const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <form name="contact-form" method="post" data-netlify="true">
+    <form
+      name="contact-form"
+      method="post"
+      data-netlify="true"
+      netlify-honeypot="bot-field"
+    >
+      <input hidden className="hidden" name="bot-field" />
       <input type="text" placeholder="name" name="name" />
       <button>Send</button>
     </form>
